6d43b10e9bf1e9412ec8d9bc02858992422d92e0
Otso Hietanen <otsohietanen1998@gmail.com>
2020-02-07 14:44:17 +0100

    Added comments and fixed minor issue of not being able to define the startlocation of player with player loc.

--- src/main/java/inf112/skeleton/app/HelloWorld.java
+++ src/main/java/inf112/skeleton/app/HelloWorld.java
@@ -16,17 +16,21 @@ import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
 import com.badlogic.gdx.math.Vector2;
 
+import javax.print.attribute.IntegerSyntax;
+
 public class HelloWorld implements ApplicationListener {
     private SpriteBatch batch;
     private BitmapFont font;
+    // Variables for Gameboard
     private TiledMap tiledMap;
+    // Board1 and 2 are not yet in use but are going to be relevant later in development
     private TiledMapTileLayer Board1;
     private TiledMapTileLayer Board2;
     private TiledMapTileLayer PlayerLayer;
     private OrthogonalTiledMapRenderer mapRenderer;
     private OrthographicCamera camera = new OrthographicCamera();
     private TmxMapLoader mapLoader;
-
+    // Variables for Player
     private TiledMapTileLayer.Cell playerWonCell = new TiledMapTileLayer.Cell();
     private TiledMapTileLayer.Cell playerDiedCell = new TiledMapTileLayer.Cell();
     private TiledMapTileLayer.Cell playerCell = new TiledMapTileLayer.Cell();
@@ -37,19 +41,19 @@ public class HelloWorld implements ApplicationListener {
         batch = new SpriteBatch();
         font = new BitmapFont();
         font.setColor(Color.MAGENTA);
+        // Code for setting up map
         tiledMap = new TmxMapLoader().load("assets/map.tmx");
-       // mapLoader.load("assets/map.tmx");
-        Board1 = (TiledMapTileLayer) tiledMap.getLayers().get("Board");
         PlayerLayer = (TiledMapTileLayer) tiledMap.getLayers().get("PlayerLayer");
         camera.setToOrtho(false, 5, 5);
         camera.update();
         mapRenderer = new OrthogonalTiledMapRenderer(tiledMap, 1/300f);
         mapRenderer.setView(camera);
-
+        // Code for defining player graphics and start location
         TextureRegion[][] spiller = new TextureRegion(new Texture("assets/player.png")).split(300,300);
         playerCell.setTile(new StaticTiledMapTile(spiller[0][0]));
         playerDiedCell.setTile(new StaticTiledMapTile(spiller[0][1]));
         playerWonCell.setTile(new StaticTiledMapTile(spiller[0][2]));
+        playerLoc = new Vector2(0,0);
     }
 
     @Override
@@ -63,8 +67,12 @@ public class HelloWorld implements ApplicationListener {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
 
+        //Renders map
         mapRenderer.render();
-        PlayerLayer.setCell(0,0, playerCell);
+        //Sets in player
+        Integer xLoc = Math.round(playerLoc.x);
+        Integer yLoc = Math.round(playerLoc.y);
+        PlayerLayer.setCell(xLoc,yLoc, playerCell);
     }
 
     @Override
