db5d650c9633eb85b971a34a2dbb2a0f51f31337
Otso Hietanen <otsohietanen1998@gmail.com>
2020-02-27 15:55:54 +0100

    Added comments Fixed the player not dying if off the board Optimized code

--- src/main/java/inf112/skeleton/app/Board.java
+++ src/main/java/inf112/skeleton/app/Board.java
@@ -6,22 +6,27 @@ import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import java.util.*;
 
 public class Board {
+    // The map itself
     TiledMap Map;
+    // Map layers
     TiledMapTileLayer Holes;
     TiledMapTileLayer Flags;
     TiledMapTileLayer Players;
 
     public Board(TiledMap Map){
+        // Takes in tiledMap from input
         this.Map = Map;
-
+        // Gets the layers from the Map
         this.Holes = (TiledMapTileLayer) Map.getLayers().get("Holes");
         this.Flags = (TiledMapTileLayer) Map.getLayers().get("Flags");
         this.Players = (TiledMapTileLayer) Map.getLayers().get("PlayerLayer");
     }
 
     public void checkForSpecialTiles(Player player){
+        // Coordinates of the player used to check for other things in the map
         Integer xLoc = player.getX();
         Integer yLoc = player.getY();
+        // Checks through maplayers for overlap and gives appropriate response
         if (Flags.getCell(xLoc, yLoc) != null) {
             player.setPlayerState(PlayerState.WINNER);
         }
@@ -29,7 +34,7 @@ public class Board {
             player.setPlayerState(PlayerState.DEAD);
         }
     }
-
+    // Methods to get elements from Board
     public TiledMap getMap() {
         return Map;
     }
