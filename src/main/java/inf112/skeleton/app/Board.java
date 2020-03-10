5040a08afd4046217f921f20083fd0aac49708c2
Otso Hietanen <otsohietanen1998@gmail.com>
2020-03-10 12:54:34 +0100

    Began Implementing special tile interactions

--- src/main/java/inf112/skeleton/app/Board.java
+++ src/main/java/inf112/skeleton/app/Board.java
@@ -1,5 +1,7 @@
 package inf112.skeleton.app;
 
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 
@@ -9,6 +11,8 @@ public class Board implements IBoard {
     // Map layers
     TiledMapTileLayer Holes;
     TiledMapTileLayer Flags;
+    TiledMapTileLayer TurnGears;
+    TiledMapTileLayer ConveyorBelts;
     TiledMapTileLayer Players;
 
     public Board(TiledMap Map){
@@ -18,6 +22,8 @@ public class Board implements IBoard {
         this.Holes = (TiledMapTileLayer) Map.getLayers().get("Holes");
         this.Flags = (TiledMapTileLayer) Map.getLayers().get("Flags");
         this.Players = (TiledMapTileLayer) Map.getLayers().get("PlayerLayer");
+        this.ConveyorBelts = (TiledMapTileLayer) Map.getLayers().get("ConveyorBelts");
+        this.TurnGears = (TiledMapTileLayer) Map.getLayers().get("TurnGears");
     }
 
     @Override
@@ -31,6 +37,33 @@ public class Board implements IBoard {
         }
         else if (Holes.getCell(xLoc, yLoc) != null){
             player.setPlayerState(PlayerState.DEAD);
+        }
+        else if (TurnGears.getCell(xLoc, yLoc) != null){
+            Integer tileId = TurnGears.getCell(xLoc, yLoc).getTile().getId();
+            // Turn right
+            if(tileId == 54){
+                player.Turn(Input.Keys.E);
+            }
+            // Turn left
+            else if(tileId == 53){
+                player.Turn(Input.Keys.Q);
+            }
+        }
+        else if (ConveyorBelts.getCell(xLoc, yLoc) != null){
+            Integer tileId = ConveyorBelts.getCell(xLoc, yLoc).getTile().getId();
+            // Yellow belts
+            if (tileId == 42 || tileId == 43 || tileId == 49 || tileId == 57 || tileId == 65 || tileId == 69){
+                player.Move(Direction.NORTH);
+            }
+            else if(tileId == 33 || tileId == 36 || tileId == 50 || tileId == 59 || tileId == 62 || tileId == 67){
+                player.Move(Direction.SOUTH);
+            }
+            else if(tileId == 35 || tileId == 41 || tileId == 52 || tileId == 58 || tileId == 61 || tileId == 66){
+                player.Move(Direction.EAST);
+            }
+            else if(tileId == 34 || tileId == 44 || tileId == 51 || tileId == 60 || tileId == 68 || tileId == 70) {
+                player.Move(Direction.WEST);
+            }
         }
     }
     // Methods to get elements from Board
--- src/main/java/inf112/skeleton/app/Game.java
+++ src/main/java/inf112/skeleton/app/Game.java
@@ -7,15 +7,10 @@ import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
 import com.badlogic.gdx.math.Vector2;
 
 
