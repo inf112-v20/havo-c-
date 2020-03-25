a74b3c442d5ecd6dae21226ea590b3726b7ac696
otsoh <otsohietanen1998@gmail.com>
2020-03-25 16:02:24 +0100

    Erased unused code 2: import boogaloo

--- src/main/java/inf112/skeleton/app/MainGameScreen.java
+++ src/main/java/inf112/skeleton/app/MainGameScreen.java
@@ -9,7 +9,6 @@ import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
--- src/main/java/inf112/skeleton/app/Robo.java
+++ src/main/java/inf112/skeleton/app/Robo.java
@@ -1,23 +1,8 @@
 package inf112.skeleton.app;
 
 
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL30;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.math.Vector2;
-
-
-import javax.print.attribute.IntegerSyntax;
 
 public class Robo extends Game {
 
