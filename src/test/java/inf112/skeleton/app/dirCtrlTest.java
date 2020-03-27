adab3981f17eda84cd57085434b2226bf08a9ccb
heineolsenhartvedt <heineolsenh@gmail.com>
2020-03-27 12:01:11 +0100

    added interfaces to Card and CardDeck, and fixed dirCtrl to DirCtrl

--- src/test/java/inf112/skeleton/app/dirCtrlTest.java
+++ src/test/java/inf112/skeleton/app/dirCtrlTest.java
@@ -1,15 +1,11 @@
 package inf112.skeleton.app;
 
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import org.junit.Before;
 import org.junit.Test;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 
 import static org.junit.Assert.assertEquals;
 
 public class dirCtrlTest {
-    dirCtrl dirCtrl = new dirCtrl();
+    DirCtrl dirCtrl = new DirCtrl();
 
     @Test
     public void turnLeftTest() {
