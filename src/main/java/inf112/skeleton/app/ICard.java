17b4eb1c4d4dcb5258df796e2ba5345ce0357edf
heineolsenhartvedt <heineolsenh@gmail.com>
2020-03-27 12:02:25 +0100

    added interfaces to Card and CardDeck

---
+++ src/main/java/inf112/skeleton/app/ICard.java
@@ -0,0 +1,9 @@
+package inf112.skeleton.app;
+
+public interface ICard {
+    void getDealt(Player newOwner);
+
+    void getDecked();
+
+    void playCard();
+}
---
+++ src/main/java/inf112/skeleton/app/ICardDeck.java
@@ -0,0 +1,10 @@
+package inf112.skeleton.app;
+
+public interface ICardDeck {
+
+    void shuffleDeck();
+
+    void dealCards(Player player);
+
+    void collectCards();
+}
