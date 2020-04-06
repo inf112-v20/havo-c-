f8fc8a324df72b6dfc1a4c9db265e9c7accd58d5
heineolsenhartvedt <heineolsenh@gmail.com>
2020-04-06 14:58:59 +0200

    implementerte moveback og turn180 i MainGameScreen

--- src/main/java/inf112/skeleton/app/MainGameScreen.java
+++ src/main/java/inf112/skeleton/app/MainGameScreen.java
@@ -26,6 +26,8 @@ public class MainGameScreen extends InputAdapter implements Screen {
     Texture move3;
     Texture moveRight;
     Texture moveLeft;
+    Texture moveback;
+    Texture turn180;
     Texture startround;
     Texture resetcards;
     Texture poweroff;
@@ -36,7 +38,7 @@ public class MainGameScreen extends InputAdapter implements Screen {
     ArrayList<Texture> cards = new ArrayList<Texture>();
     // Temp Arraylist Random numbers
     ArrayList<Integer> cardNumbers = new ArrayList<Integer>();
-    // Temp this array contain the numbers 1 to 5
+    // Temp this array contain the numbers 1 to 7
     ArrayList<Texture> pickedCards = new ArrayList<Texture>();
 
     // Temp Var for player, I think this variable should be put into player class
@@ -66,6 +68,8 @@ public class MainGameScreen extends InputAdapter implements Screen {
         move3 = new Texture("assets/move3.png");
         moveRight = new Texture("assets/moveright.png");
         moveLeft = new Texture("assets/moveleft.png");
+        moveback = new Texture("assets/moveback.png");
+        turn180 = new Texture("assets/turn180.png");
         startround = new Texture("assets/startround1.png");
         resetcards = new Texture("assets/resetcards1.png");
         poweroff = new Texture("assets/poweroff1.png");
@@ -176,7 +180,7 @@ public class MainGameScreen extends InputAdapter implements Screen {
         // Insert delay here
         deck.dealCards(player);
         // Insert delay here that allows players to choose their cards
-        for(Integer i = 0; i<5; i++){
+        for(Integer i = 0; i<7; i++){
             // Must be improved to make card priority a thing
             player.playHand(i);
             // Must be improved so that the different parts act in the correct order
@@ -235,7 +239,7 @@ public class MainGameScreen extends InputAdapter implements Screen {
     public void loadCards() {
         cardNumbers.clear();
         Random random = new Random();
-        int upperbound = 5;
+        int upperbound = 7;
         int randomNumber;
         for (int i = 0; 9 > i; i++) {
             randomNumber = random.nextInt(upperbound);
@@ -257,6 +261,12 @@ public class MainGameScreen extends InputAdapter implements Screen {
             }
             else if (cardNumbers.get(i) == 4) {
                 cards.add(moveLeft);
+            }
+            else if (cardNumbers.get(i) == 5) {
+                cards.add(moveback);
+            }
+            else if (cardNumbers.get(i) == 6) {
+                cards.add(turn180);
             }
 
         }
@@ -318,6 +328,17 @@ public class MainGameScreen extends InputAdapter implements Screen {
         }
         else if (cardNumbers.get(cardXY) == 4) {
             player.Turn(TurnDirection.LEFT);
+        }
+        else if (cardNumbers.get(cardXY) == 5) {
+            player.Turn(TurnDirection.RIGHT);
+            player.Turn(TurnDirection.RIGHT);
+            player.Move(player.getPlayerDir());
+            player.Turn(TurnDirection.RIGHT);
+            player.Turn(TurnDirection.RIGHT);
+        }
+        else if (cardNumbers.get(cardXY) == 6) {
+            player.Turn(TurnDirection.RIGHT);
+            player.Turn(TurnDirection.RIGHT);
         }
 
         gameBoard.checkForSpecialTiles(player, Boolean.FALSE);
